431b41c5102aa8ac7e21199c540b6da3bc40b7b3
Tadahito Yao <eighttails@nifty.com>
2017-08-26 22:49:54 +0900

    実行ごとにリビルドするようにした

--- tesseract/tesseract.sh
+++ tesseract/tesseract.sh
@@ -19,7 +19,7 @@ $MINGW_PACKAGE_PREFIX-icu
 }
 
 function build(){
-if [ -e $PREFIX/bin/tesseract.exe -a $((FORCE_INSTALL)) == 0 ]; then
+if [ -d $PREFIX/bin/tesseract.exe -a $((FORCE_INSTALL)) == 0 ]; then
 echo "tesseract is already installed."
 exit 0
 fi
@@ -27,19 +27,22 @@ fi
 #TESSERACT_VERSION=4.00.00alpha
 TESSERACT_SRC_DIR=tesseract-$MINGW_CHOST
 
-if [ ! -d  $TESSERACT_SRC_DIR ]; then
+if [ ! -e  $TESSERACT_SRC_DIR ]; then
 git clone https://github.com/tesseract-ocr/tesseract.git $TESSERACT_SRC_DIR
 fi
 
 pushd $TESSERACT_SRC_DIR
 git pull
 
+if [ -e Makefile ]; then
+make clean
+fi
+
 ./autogen.sh
 exitOnError
 
 export LIBLEPT_HEADERSDIR=$PREFIX/include/leptonica
 
-CPPFLAGS='-DUSE_STD_NAMESPACE' \
 ./configure \
 --build=$MINGW_CHOST \
 --host=$MINGW_CHOST \
